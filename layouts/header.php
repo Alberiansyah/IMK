3ab6dec05da1f2ad6ea08c9bf5f80df618d4eaa9
Alberiansyah <alberiansyah1@gmail.com>
2023-02-07 22:23:26 +0700

    Update Build -> 2.1

--- layouts/header.php
+++ layouts/header.php
@@ -36,7 +36,6 @@
                 <li class="dropdown nav-item d-md-block">
                     <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img src="<?= $hostToRoot ?>/wp-content/assets/images/avatars/avatar1.png" alt="" class="rounded-circle"></a>
                     <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <li><a href="#">Account Settings</a></li>
                         <li><a href="<?= $hostToRoot ?>logout">Log Out</a></li>
                     </ul>
                 </li>
